3b957c89cedc2621740392fe2c4246991e483066
baiyong <baiyonguestc@gmail.com>
2024-11-20 09:50:03 +0800

    Add sequence and corpus utility functions

--- genomix/utils/__init__.py
+++ genomix/utils/__init__.py
@@ -28,5 +28,7 @@ from ._model_utils import load_config_hf, load_state_dict_hf, model_size
 from ._common_utils import check_dir_exists, check_file_exists, partition, read_json, read_txt, write_json
 from ._vocab_utils import update_vocab
 from ._constants import SPECIAL_TOKENS, TOKENIZER_MODELS, INITIAL_ALPHABETS
+from ._sequence_utils import chunk_sequence, down_sampling, batch_iterator
+from ._corpus_utils import generate_corpus_txt
 
 
