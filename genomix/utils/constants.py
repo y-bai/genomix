9ce9d56edda71f49fa1ccd89a9489090188bd841
baiyong <baiyonguestc@gmail.com>
2024-11-22 17:36:27 +0800

    fix  circular import

--- genomix/utils/constants.py
+++ genomix/utils/constants.py
@@ -25,7 +25,10 @@
 
 from enum import Enum
 import os
-from .common import check_dir_exists
+
+def _make_dir_if_not_exists(dir_path):
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
 
 class ExtendedEnum(Enum):
     @classmethod
@@ -56,18 +59,15 @@ SPM_VOCAB_MODEL_PREFIX = "spm_vocab"
 # initial alphabet to be used for BPE/UNIGRAM tokneizer training
 INITIAL_ALPHABETS = list("ACGTN")
 
-GENOMIX_HOME = check_dir_exists(
-    os.getenv('GENOMIX_HOME', os.path.expanduser('~/.genomix')), 
-    create=True)
-GENOMIX_CACHE_DIR = check_dir_exists(
-    os.path.join(os.path.expanduser('~/.cache'), 'genomix'), 
-    create=True)
-GENOMIX_CACHE_DATA_DIR = check_dir_exists(
-    os.path.join(GENOMIX_CACHE_DIR, 'data'), 
-    create=True)
-GENOMIX_CACHE_OTHER_DIR = check_dir_exists(
-    os.path.join(GENOMIX_CACHE_DIR, 'other'), 
-    create=True)
+GENOMIX_HOME = os.getenv('GENOMIX_HOME', os.path.expanduser('~/.genomix'))
+GENOMIX_CACHE_DIR = os.path.join(os.path.expanduser('~/.cache'), 'genomix')
+GENOMIX_CACHE_DATA_DIR = os.path.join(GENOMIX_CACHE_DIR, 'data')
+GENOMIX_CACHE_OTHER_DIR = os.path.join(GENOMIX_CACHE_DIR, 'other')
+_make_dir_if_not_exists(GENOMIX_HOME)
+_make_dir_if_not_exists(GENOMIX_CACHE_DIR)
+_make_dir_if_not_exists(GENOMIX_CACHE_DATA_DIR)
+_make_dir_if_not_exists(GENOMIX_CACHE_OTHER_DIR)
+
 
 # the size to be viewed as large file
 LARGE_FILE_SIZE = 1024 * 1024 * 500  # 500MB
@@ -76,4 +76,6 @@ LARGE_FILE_SIZE = 1024 * 1024 * 500  # 500MB
 BATCH_NUM_SEQS = 10000
 
 SEQENCE_FEATURE_NAME_IN_DATASET_CHUNKED = "chunked_sequence"
+
+
 
